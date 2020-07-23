9aba11a2bbf4cbe9eada6a722c0494c18f07b172
Luís Lourenço <luis.lourenco.1510@tecnico.ulisboa.pt>
2020-07-23 19:29:09 +0100

    mme

--- luis/memes/main.cpp
+++ luis/memes/main.cpp
@@ -9,6 +9,7 @@ using namespace std::chrono;
 #include <TGraph2D.h>
 #include <TCanvas.h>
 #include <TAxis.h>
+#include <TMultiGraph.h>
 
 using namespace std;
 
@@ -27,6 +28,7 @@ int main(int argc, char** argv){
 	form.push_back(TFormula("","x[3]"));
 	form.push_back(TFormula("","x[4]"));
 	form.push_back(TFormula("","x[5]"));
+	//polarizacao circular
 	form.push_back(TFormula("","-pow(1-x[3]*x[3]-x[4]*x[4]-x[5]*x[5], 3/2)*(-x[4]*sin(x[0]-x[6])/sqrt(2)+x[5]*cos(x[0]-x[6])/sqrt(2))"));
 	form.push_back(TFormula("","-pow(1-x[3]*x[3]-x[4]*x[4]-x[5]*x[5], 3/2)*(-sin(x[0]-x[6])/sqrt(2)+x[3]*sin(x[0]-x[6])/sqrt(2))"));
 	form.push_back(TFormula("","-pow(1-x[3]*x[3]-x[4]*x[4]-x[5]*x[5], 3/2)*(cos(x[0]-x[6])/sqrt(2)-x[3]*cos(x[0]-x[6])/sqrt(2))"));
@@ -35,7 +37,7 @@ int main(int argc, char** argv){
 
 	auto start = high_resolution_clock::now(); 
 
-	vector<ODEpoint> solucao = solver.RK4solver(point0, 0, 100, 0.01);
+	vector<ODEpoint> solucao = solver.RK4solver(point0, 0, 200, 0.001);
 
 	auto stop = high_resolution_clock::now(); 
 	auto duration = duration_cast<microseconds>(stop - start); 
@@ -45,18 +47,33 @@ int main(int argc, char** argv){
 	int n = solucao.size();
 	TGraph* g1 = new TGraph(n);
 	TGraph* g2 = new TGraph(n);
+	TGraph* g3 = new TGraph(n);
+	TGraph* g4 = new TGraph(n);
+	TGraph* g5 = new TGraph(n);
 	for(int i=0; i<n; i++){
 		g1->SetPoint(i, solucao[i].Get_VarTime()[0], solucao[i].Get_VarTime()[1]);
 		g2->SetPoint(i, solucao[i].Get_VarTime()[0], solucao[i].Get_VarTime()[2]);
+		g3->SetPoint(i, solucao[i].Get_VarTime()[6], solucao[i].Get_VarTime()[0]);
+		g4->SetPoint(i, solucao[i].Get_VarTime()[6], solucao[i].Get_VarTime()[1]);
+		g5->SetPoint(i, solucao[i].Get_VarTime()[6], solucao[i].Get_VarTime()[2]);
 	}
 
 	g1->SetMarkerColor(kBlue);
 	g1->SetLineColor(kBlue);
 	g2->SetMarkerColor(kBlue);
 	g2->SetLineColor(kBlue);
+	g3->SetMarkerColor(kBlue);
+	g3->SetLineColor(kBlue);
+	g4->SetMarkerColor(kBlue);
+	g4->SetLineColor(kBlue);
+	g5->SetMarkerColor(kViolet);
+	g5->SetLineColor(kViolet);
 
 	g1->SetTitle(";x;y");
 	g2->SetTitle(";x;z");
+	g3->SetTitle(";t;x");
+	g4->SetTitle(";t;y");
+	g5->SetTitle(";t;z");
 
 	TCanvas* c1 = new TCanvas();	
 	g1->Draw("APL");
@@ -66,12 +83,29 @@ int main(int argc, char** argv){
 	g2->Draw("APL");
 	c2->SaveAs("plot2.png");
 
+	TCanvas* c3 = new TCanvas();	
+	g3->Draw("APL");
+	c3->SaveAs("plot3.png");
+
+	TCanvas* c4 = new TCanvas();
+	TMultiGraph* mg1 = new TMultiGraph();
+	mg1->Add(g4);
+	mg1->Add(g5);	
+	mg1->SetTitle(";t;y,z");
+	mg1->Draw("APL");
+	c4->SaveAs("plot4.png");
 
 	delete[] p0;
 	delete g1;
 	delete g2;
+	delete g3;
+	delete g4;
+	delete g5;
 	delete c1;
 	delete c2;
+	delete c3;
+	delete c4;
+	delete mg1;
 
   	return 0;
 }

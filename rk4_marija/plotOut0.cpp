d901d14fd507606cf8878c835e02c205bd032b6d
Luís Lourenço <luis.lourenco.1510@tecnico.ulisboa.pt>
2020-08-07 13:41:06 +0100

    vrvuivy

--- rk4_marija/plotOut0.cpp
+++ rk4_marija/plotOut0.cpp
@@ -3,37 +3,44 @@
 
 void plotOut0(){
 
-	double p01, p02, p03, Eo, delta, kdamp, k, T, xgrid, ygrid;
+	double x01, x02, x03, p01, p02, p03, Eo, delta, T;
 	long long int N;
 	int pri;
+	char trash[64];
 	FILE *foo;
-	foo=fopen("InputTotBatch.txt","r");
-	fscanf(foo,"%lf %lf %lf %lf %lf %lf %lf %lf %lld %i %lf %lf ", &p01, &p02, &p03, &Eo, &delta, &kdamp,&k, &T, &N, &pri, &xgrid, &ygrid);
+	foo=fopen("InputToBatch.txt","r");
+	fscanf(foo,"%s %lf %lf %lf %lf %lf %lf %lf %lli %i ", 
+				trash, &x01, &x02, &x03, &p01, &p02, &p03, &T, &N, &pri);
 	fclose(foo);
+
 
 	// t x y z px py pz gama
 	vector<vector<double>> sol = ReadFile2Vec("Out0.txt");
 	int n = sol.size();
 
+	delta = 0.7071;
+	Eo = 10;
 
-	/*
+
+	double avgx=0;
 	TGraph2D* trajetoria = new TGraph2D(n);
 	for (int i=0; i<n; i++){
-		if (sol[i][0]>100. && sol[i][0]<150.)
+		avgx +=sol[i][1]/(double)n;
 		trajetoria->SetPoint(i, sol[i][1], sol[i][2], sol[i][3]);
 	}
+	cout<<avgx<<endl;
+	TGraph2D* momentos = new TGraph2D(n);
+	for (int i=0; i<n; i++){
+		momentos->SetPoint(i, sol[i][4], sol[i][5], sol[i][6]);
+	}		
 
-	TGraph2D* trajetoria_teorica = new TGraph2D(n);
+	//LAB FRAME
+	/*TGraph2D* trajetoria_teorica = new TGraph2D(n);
 	for (int i=0; i<n; i++){
 		trajetoria_teorica->SetPoint(i, Eo*Eo/4.*(sol[i][0]-sol[i][1]+(delta*delta-1./2.)*sin(2.*(sol[i][0]-sol[i][1]))),
 										delta*Eo*sin(sol[i][0]-sol[i][1]),
 										-sqrt(1-delta*delta)*Eo*cos(sol[i][0]-sol[i][1]));
 	}
-
-	TGraph2D* momentos = new TGraph2D(n);
-	for (int i=0; i<n; i++){
-		momentos->SetPoint(i, sol[i][4], sol[i][5], sol[i][6]);
-	}	
 
 	TGraph2D* momentos_teoricos = new TGraph2D(n);
 	for (int i=0; i<n; i++){
@@ -41,26 +48,44 @@ void plotOut0(){
 										delta*Eo*cos(sol[i][0]-sol[i][1]),
 										sqrt(1-delta*delta)*Eo*sin(sol[i][0]-sol[i][1]));
 	}*/
+	//REST FRAME
+	TPolyLine3D* trajetoria_teorica = new TPolyLine3D(n);
+	double gama0 = sqrt(1.+Eo*Eo/2.);
+	double q = Eo/2./gama0;
+	for (int i=0; i<n; i++){
+		trajetoria_teorica->SetPoint(i, avgx+(delta*delta-0.5)*q*q*sin(2*(sol[i][0]-sol[i][1])),
+										2.*delta*q*sin(sol[i][0]-sol[i][1]),
+										-2*sqrt(1-delta*delta)*q*cos(sol[i][0]-sol[i][1]));
+	}
+	TGraph2D* momentos_teoricos = new TGraph2D(n);
+	for (int i=0; i<n; i++){
+		momentos_teoricos->SetPoint(i, (2*delta*delta-1)*Eo*Eo/4./gama0*cos(2*(sol[i][0]-sol[i][1])),
+										delta*Eo*cos(sol[i][0]-sol[i][1]),
+										sqrt(1-delta*delta)*Eo*sin(sol[i][0]-sol[i][1]));
+	}
 
-
+	/*
+	double avgx=0;
 
 	TGraph* trajetoria = new TGraph(n);
 	for (int i=0; i<n; i++){
+		avgx += sol[i][1]/(double)n;
 		trajetoria->SetPoint(i, sol[i][1], sol[i][2]);
 	}
 
-	TGraph* trajetoria_teorica = new TGraph(n);
-	for (int i=0; i<n; i++){
-		trajetoria_teorica->SetPoint(i, Eo*Eo/4.*(sol[i][0]-sol[i][1]+(delta*delta-1./2.)*sin(2.*(sol[i][0]-sol[i][1]))),
-										delta*Eo*sin(sol[i][0]-sol[i][1])
-										);
-	}
 
 	TGraph* momentos = new TGraph(n);
 	for (int i=0; i<n; i++){
 		momentos->SetPoint(i, sol[i][4], sol[i][5]);
 	}	
 
+	//LAB FRAME 
+	TGraph* trajetoria_teorica = new TGraph(n);
+	for (int i=0; i<n; i++){
+		trajetoria_teorica->SetPoint(i, Eo*Eo/4.*(sol[i][0]-sol[i][1]+(delta*delta-1./2.)*sin(2.*(sol[i][0]-sol[i][1]))),
+										delta*Eo*sin(sol[i][0]-sol[i][1])
+										);
+	}
 	TGraph* momentos_teoricos = new TGraph(n);
 	for (int i=0; i<n; i++){
 		momentos_teoricos->SetPoint(i, Eo*Eo/4.*(1.+(2.*delta*delta-1.)*cos(2.*(sol[i][0]-sol[i][1]))),
@@ -68,18 +93,43 @@ void plotOut0(){
 										);
 	}
 
+	//REST FRAME
+	TGraph* trajetoria_teorica = new TGraph(n);
+	double gama0 = sqrt(1.+Eo*Eo/2.);
+	double q = Eo/2./gama0;
+	for (int i=0; i<n; i++){
+		trajetoria_teorica->SetPoint(i, (delta*delta-0.5)*q*q*sin(2*(sol[i][0]-sol[i][1])),
+										2*delta*q*sin(sol[i][0]-sol[i][1])
+										);
+	}
+	TGraph* momentos_teoricos = new TGraph(n);
+	for (int i=0; i<n; i++){
+		momentos_teoricos->SetPoint(i, (2*delta*delta-1)*Eo*Eo/4./gama0*cos(2*(sol[i][0]-sol[i][1])),
+										delta*Eo*cos(sol[i][0]-sol[i][1])
+										);
+	}
+	*/
 
 	trajetoria->SetTitle(";x;y;z");
-	trajetoria->SetMarkerColor(kRed);
-	trajetoria_teorica->SetMarkerColor(kBlue);
-	trajetoria_teorica->SetLineColor(kBlue);
+	trajetoria->SetMarkerColor(kPink);
+	//trajetoria_teorica->SetMarkerColor(kAzure+9);
+	//trajetoria_teorica->SetLineColor(kAzure+9);
+	trajetoria_teorica->SetLineColorAlpha(kAzure+9,1);
+	trajetoria_teorica->SetLineWidth(4);
 
 	momentos->SetTitle(";px;py;pz");
-	momentos->SetMarkerColor(kRed);
-	momentos_teoricos->SetMarkerColor(kBlue);
-	momentos_teoricos->SetLineColor(kBlue);
+	momentos->SetMarkerColor(kPink);
+	momentos_teoricos->SetMarkerColor(kAzure+9);
+	momentos_teoricos->SetLineColor(kAzure+9);
 
-
+	trajetoria->GetXaxis()->SetMaxDigits(3);
+	//trajetoria->GetXaxis()->SetRangeUser(avgx-3, avgx+3);
+	//trajetoria_teorica->SetMarkerStyle(8);
+	//trajetoria_teorica->SetMarkerSize(0.5);
+	momentos_teoricos->SetMarkerStyle(8);
+	momentos_teoricos->SetMarkerSize(0.5);
+	trajetoria->GetXaxis()->SetTitleOffset(1.5);
+	trajetoria->GetYaxis()->SetTitleOffset(0.5);
 
 	/*
 	trajetoria->SetMinimum(-1.);
@@ -94,17 +144,37 @@ void plotOut0(){
 	*/
 
 
-	TCanvas* c1 = new TCanvas("c1", "", 2400, 1200);
-	c1->Divide(2,1);
+	TCanvas* c1 = new TCanvas("c1", "", 1800, 600);
+	c1->Divide(3,1);
+
+	c1->cd(1)->SetPad(0, 0, 0.45, 1);
+	c1->cd(2)->SetPad(0.44, 0, 0.9, 1);
+	c1->cd(3)->SetPad(0.9, 0, 1, 1);
 
 	c1->cd(1);
-	trajetoria->Draw("AP");
-	//trajetoria_teorica->Draw("SAME");
+	c1->cd(1)->SetGrid();
+	trajetoria->Draw("P");
+	trajetoria_teorica->Draw("SAME4");
 
 	c1->cd(2);
-	momentos->Draw("AP");
-	//momentos_teoricos->Draw("SAME");
+	c1->cd(2)->SetGrid();
+	momentos->Draw("P");
+	momentos_teoricos->Draw("SAME P");
 
+
+
+	TLatex* t[5];
+	t[0] = new TLatex(.0, .8, "a_{0} = 10");
+	t[1] = new TLatex(.0, .77, "delta = 0.7071");
+	t[2] = new TLatex(.0, .74, "#vec{x}_{0}=0, #vec{p}_{0}=-3.5#hat{x}");
+	t[3] = new TLatex(.0, .71, "t_{subida}=20pi");
+	t[4] = new TLatex(.0, .68, "t_{stable}=300");
+
+	for(int i=0; i<5; i++) t[i]->SetTextSize(0.1);
+
+	c1->cd(3);
+	for(int i=0; i<5; i++)
+		t[i]->Draw();
 
 	/*
 	momentos->GetXaxis()->SetTitleOffset(1.75);

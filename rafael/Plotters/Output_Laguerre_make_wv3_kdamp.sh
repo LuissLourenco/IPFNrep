6dcd58e7f16e77f5acbafa2b490d4b05b034bdd6
Rafael Russo Almeida <rafaelrussoalmeida@tecnico.ulisboa.pt>
2020-09-01 20:20:26 +0100

    meme

---
+++ rafael/Plotters/Output_Laguerre_make_wv3_kdamp.sh
@@ -0,0 +1,3 @@
+g++ -o2 Output_Laguerre_wv3_kdamp.cpp -lm -o Output_Laguerre_wv3_kdamp `root-config --cflags --glibs`
+./Output_Laguerre_wv3_kdamp
+xdg-open Output_Laguerre_wv3_kdamp_data/Output_wv3_l1p0_px-2000.000000.png

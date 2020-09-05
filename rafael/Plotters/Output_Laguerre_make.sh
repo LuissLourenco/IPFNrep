13d2acb78d004ea00632aa015834cf6616913d03
Rafael Russo Almeida <rafaelrussoalmeida@tecnico.ulisboa.pt>
2020-09-05 11:53:16 +0100

    emem

--- rafael/Plotters/Output_Laguerre_make.sh
+++ rafael/Plotters/Output_Laguerre_make.sh
@@ -1,6 +1,7 @@
 
 g++ -o2 Output_Laguerre_v$1.cpp -lm -o Output_Laguerre_v$1 `root-config --cflags --glibs`
 ./Output_Laguerre_v$1
+rm Output_Laguerre_v$1
 xdg-open Output_Laguerre_v$1_data/$2
 
 # v1 - Laguerre Gaussian without Bx, px=0

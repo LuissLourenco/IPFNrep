9a18e321e062202108fa426d3861493ea12e18e7
Luís Lourenço <luis.lourenco.1510@tecnico.ulisboa.pt>
2020-09-09 03:04:37 +0100

    AAA

--- rk4_cooling/plotOut3.cpp
+++ rk4_cooling/plotOut3.cpp
@@ -2,7 +2,6 @@
 
 void plotOut3(){
 
-
 	FILE *foo;
 	char trash[128];
 	double x01,x02,x03,p01,p02,p03, kdamp, T;
@@ -17,15 +16,12 @@ void plotOut3(){
 	fclose(foo);
 
 
-
-
-
 	int n_cols, n_points;
 	double** values = ReadFile("Out3.txt", &n_cols, &n_points, false);
 
 	TGraph* graph_y = GetTGraph(DataSet(n_points, values[1]), DataSet(n_points, values[2]));
 	TGraph* graph_p = GetTGraph(DataSet(n_points, values[4]), DataSet(n_points, values[5]));
-		
+
 	TGraph2D* graph_y2 = new TGraph2D(n_points,values[1],values[2],values[3]);
 	TGraph2D* graph_p2 = new TGraph2D(n_points,values[4],values[5],values[6]);
 
@@ -46,7 +42,21 @@ void plotOut3(){
 	graph_y2->SetMarkerColor(kRed);
 	graph_p2->SetMarkerColor(kRed);
 
-	
+	TH2F* h2 = new TH2F("h2", ";x;y;z", 1, 0, 5, 1, -15, 15);
+	graph_y2->SetHistogram(h2);
+	graph_y2->SetMinimum(-15);
+	graph_y2->SetMaximum(15);
+
+	graph_y2->GetXaxis()->SetLabelSize(0.04);
+	graph_y2->GetYaxis()->SetLabelSize(0.04);
+	graph_y2->GetZaxis()->SetLabelSize(0.04);
+	graph_y2->GetXaxis()->SetTitleSize(0.05);
+	graph_y2->GetYaxis()->SetTitleSize(0.05);
+	graph_y2->GetZaxis()->SetTitleSize(0.05);
+	graph_y2->GetXaxis()->SetTitleOffset(1.4);
+	graph_y2->GetYaxis()->SetTitleOffset(1.4);
+	graph_y2->GetZaxis()->SetTitleOffset(1.1);
+	gStyle->SetOptStat(0);
 
 	TCanvas* c1 = new TCanvas("c1", (string("wave_type=")+to_string(wave_type)).c_str(), 1500, 1000);
 	c1->Divide(2, 1);	
@@ -56,9 +66,17 @@ void plotOut3(){
 	c1->cd(2);
 	//graph_p->Draw("AP");
 	graph_p2->Draw("P");
+	c1->cd(1);
+
+	gPad->WaitPrimitive();
+	gPad->SaveAs("Plot.png");
 
 
-	graph_y2->GetZaxis()->SetRange(-10,10);
+	delete graph_y;
+	delete graph_p;
+	delete h2;
+	delete graph_y2;
+	delete graph_p2;
+	delete c1;
 
-	c1->SaveAs("Plot.png");
 }
